a6b0cbc7ce748ce06d3e8e53bf7e6d7821873442
Gabriel Krotkov <gabriel.krotkov@fda.hhs.gov>
2025-03-18 10:08:52 -0400

    size adjustment

--- analyses/winner_seed_distribution.R
+++ analyses/winner_seed_distribution.R
@@ -4,7 +4,7 @@ library(scoutR) # devtools::install_github("gkrotkov/scoutR")
 
 winner_seed_pct_thru_wk <- function(yr, wk){
     wks <- 1:wk
-    lst <- lapply(wks, week_winning_seed_table, yr)
+    lst <- lapply(wks, week_winning_seed_table, year = yr, size = 8)
     result <- purrr::reduce(lst, combine_tbls)
     dummy <- rep(0, 8)
     names(dummy) <- 1:8
@@ -34,7 +34,7 @@ ggplot(df, aes(x = year, y = win_pct, color = seed, group = seed)) +
     geom_line() + 
     geom_point() + 
     labs(title = "#1 Seeds are crushing in 2025",
-         subtitle = "Cumulative through week 3",
+         subtitle = "Cumulative through week 3, 8-alliance events only",
          x = "Year", y = "Tournament Win Percentage", 
          color = "Seed", caption = "Data from frc-events via TBA") +
     theme_bw() + 
