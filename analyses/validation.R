210410c6dff6e9fd3cb714524e06afdacc8bdd1d
Ghostyaq <mitchellhung176@gmail.com>
2025-04-13 19:59:14 -0400

    Updates from the 7-8 session, got a string of problematic matches to file through

--- analyses/validation.R
+++ analyses/validation.R
@@ -2,6 +2,7 @@
 
 library(scoutR) # devtools::install_github("gkrotkov/scoutR")
 
+
 data <- readr::read_csv("shinyapp/data_files/dchamp/data.csv")
 
 event_code <- "2025chcmp"
@@ -16,7 +17,7 @@ tba <- event_matches(event_code, match_type = "qual")
 schedule <- qual_schedule(event_code) |>
     tidyr::pivot_longer(red1:blue3) |>
     dplyr::mutate(
-        robot = case_when(
+        robot = dplyr::case_when(
             name == "red1" ~ "R1",
             name == "red2" ~ "R2",
             name == "red3" ~ "R3",
@@ -24,15 +25,35 @@ schedule <- qual_schedule(event_code) |>
             name == "blue2" ~ "B2",
             name == "blue3" ~ "B3"
         ),
-        team = substr(value, 4, nchar(value)),
-        match = match_number
+        team = value,
+        match = match_number,
+        string = paste(match_number, robot, value, sep = "_")
     ) |>
     dplyr::select(
-        match, robot, team
+        match, robot, team, string
     )
+
+data$string <- paste(data$match, data$robot, data$team, sep = "_")
+
+temp1 <- rep(NA, nrow(data)) #35 datas don't exist in schedule
+for (i in seq_len(nrow(data))){
+    temp1[i] = !(data$string[i] %in% schedule$string)
+}
+
+temp2 <- rep(NA, nrow(data)) #47 schedules don't exist in data
+for (i in seq_len(nrow(schedule))){
+    temp2[i] = !(schedule$string[i] %in% data$string)
+}
+
+cat("Here is a list of all the data rows we have that are not scheduled: ",
+    data$string[which(temp1)])
+
+cat("Here is a list of all the scheduled matches we did not cover in our data: ",
+    schedule$string[which(temp2)])
 
 ############################
 #### Correctness Checks ####
-############################    
+############################ 
+
 
 
